d4a0e0fb04344b09a95dfc64844f74bafb0d11a6
Benny <bennylin050@gmail.com>
2023-09-26 12:31:45 -0400

    a

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class TipCalculator {
+import java.util.Scanner;
+public class TipCalculator {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is the total bill?: ");
+        double total = scan.nextDouble();
+
+        System.out.println("What percentage of tip do you want to give?: ");
+        int tipPercent = scan.nextInt();
+
+        System.out.println("What is the total number of people?: ");
+        int totalPeople = scan.nextInt();
+
+    }
 }
